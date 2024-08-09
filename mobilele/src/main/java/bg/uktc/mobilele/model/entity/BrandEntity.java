903fab284750f470b27753712a03b289ad8f1c79
daniel <dani.dimitrov.snack9@gmail.com>
2024-08-09 13:41:00 +0300

    start of mobilele

---
+++ mobilele/src/main/java/bg/uktc/mobilele/model/entity/BrandEntity.java
@@ -0,0 +1,13 @@
+package bg.uktc.mobilele.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "brands")
+public class BrandEntity extends BaseEntity{
+    private String name;
+
+
+
+}
